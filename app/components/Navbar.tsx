814b78a98931e651b58dbc9b989b5c0f6081d1cd
Sushank <sushanklamichhane12@gmail.com>
2026-02-05 08:48:03 +0545

    Update portfolio layout and styles; add Navbar component with GSAP animations

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,105 @@
+import gsap from "gsap";
+
+import { SplitText } from "gsap/SplitText";
+import { Menu, X } from "lucide-react";
+
+import { useEffect, useRef, useState } from "react";
+
+const Navbar = () => {
+  const mobileMenuRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    gsap.registerPlugin(SplitText);
+    if (isMenuOpen) {
+      gsap.to(mobileMenuRef.current, {
+        duration: 0.5,
+        right: "0%",
+        ease: "power3.inOut",
+      });
+    } else {
+      gsap.to(mobileMenuRef.current, {
+        duration: 0.5,
+        right: "-100%",
+        ease: "power3.inOut",
+      });
+    }
+    gsap.to( ".navlinks", {
+      duration: 1,
+      opacity: isMenuOpen ? 1 : 0,
+      scale: isMenuOpen ? 1 : 0.8,
+      stagger: 0.1,
+      ease: "power3.inOut",
+
+    })
+  }, [isMenuOpen]);
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const target = e.currentTarget;
+    const split = SplitText.create(target, {
+      type: "chars",
+    });
+    gsap.from(split.chars, {
+      duration: 0.4,
+      y: -8,
+      opacity: 0,
+      stagger: 0.05,
+      ease: "back.out(1.7)",
+    });
+  };
+
+  return (
+    <nav className="p-6 navbar fixed top-0 left-0 w-full flex justify-between items-center z-50  bg-opacity-50">
+      {/* Navbar content goes here */}
+      <div className="left-section">
+        <div className="flex gap-2 text-xl  font-[Kalam] font-normal  text-white">
+          <p className="">||</p>
+          <h2> सुशांक </h2>
+          <p>||</p>
+        </div>
+      </div>
+      <div className="right-section md:flex hidden gap-8 text-white font-semibold">
+        <a onMouseEnter={handleMouseEnter} href="">
+          About me
+        </a>
+        <a onMouseEnter={handleMouseEnter} href="">
+          Services
+        </a>
+        <a onMouseEnter={handleMouseEnter} href="">
+          Projects
+        </a>
+        <a onMouseEnter={handleMouseEnter} href="">
+          Contact
+        </a>
+      </div>
+
+      {/* Mobile menu icon can go here */}
+      <Menu className="md:hidden hover:scale-105 " onClick={() => setIsMenuOpen(!isMenuOpen)} />
+      <div
+        ref={mobileMenuRef}
+        className="menuitems flex flex-col  absolute top-0 
+         bg-gray-900 bg-opacity-70  rounded-lg gap-4 w-screen h-screen -right-full text-white font-semibold"
+      >
+        <div className="navlinks flex flex-col gap-8  text-3xl justify-center h-full w-full items-center">
+          <div className="absolute top-8 left-8">
+        <div className="flex gap-2 text-xl  font-[Kalam] font-normal  text-white">
+          <p className="">||</p>
+          <h2> सुशांक </h2>
+          <p>||</p>
+        </div>
+      </div>
+          <X className=" absolute top-8 right-8 cursor-pointer z-50" onClick={(e) => { e.stopPropagation(); setIsMenuOpen(false); }} />
+         
+         <div className="navlinks flex flex-col gap-8  text-3xl justify-center h-full w-full items-center">
+          <a href="">About me</a>
+          <a href="">Services</a>
+          <a href="">Projects</a>
+          <a href="">Contact</a>
+          </div> 
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+"use client"
+import gsap from 'gsap'
+import { ReactLenis, LenisRef } from 'lenis/react'
+import { useEffect, useRef } from 'react'
+import Navbar from './components/Navbar'
 
 export default function Home() {
+  const lenisRef = useRef<LenisRef>(null)
+  
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+  
+    gsap.ticker.add(update)
+  
+    return () => gsap.ticker.remove(update)
+  }, [])
+  
   return (
-    <>
-   <h1>Hello</h1>
-    </>
-  );
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+      <Navbar />
+      
+    </ReactLenis>
+  )
 }
